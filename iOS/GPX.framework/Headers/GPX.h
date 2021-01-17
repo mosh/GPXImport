a7eed7446d32f80b35f0efccd30ae43d4ceebf02
John Moshakis <john.moshakis@firmex.com>
2021-01-17 15:57:17 -0500

    Import of GPX

---
+++ iOS/GPX.framework/Headers/GPX.h
@@ -0,0 +1,39 @@
+//
+//  GPX.h
+//  GPX
+//
+//  Created by John Moshakis on 1/3/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "GPXParser.h"
+#import "GPXConst.h"
+#import "GPXType.h"
+#import "GPXRoot.h"
+#import "GPXMetadata.h"
+#import "GPXWaypoint.h"
+#import "GPXRoutePoint.h"
+#import "GPXTrackPoint.h"
+#import "GPXRoute.h"
+#import "GPXTrack.h"
+#import "GPXExtensions.h"
+#import "GPXTrackSegment.h"
+#import "GPXCopyright.h"
+#import "GPXLink.h"
+#import "GPXEmail.h"
+#import "GPXPerson.h"
+#import "GPXAuthor.h"
+#import "GPXPoint.h"
+#import "GPXPointSegment.h"
+#import "GPXBounds.h"
+
+//! Project version number for GPX.
+FOUNDATION_EXPORT double GPXVersionNumber;
+
+//! Project version string for GPX.
+FOUNDATION_EXPORT const unsigned char GPXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPX/PublicHeader.h>
+
+
